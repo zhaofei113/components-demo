ba4845f278092b07178868a09efa98b30cdb500e
zhaofei <zhaofei@missfresh.cn>
2019-07-29 20:51:17 +0800

    rant

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <z-progress width="200" percent="20"></z-progress>
+    <!-- <z-progress width="200" percent="20"></z-progress> -->
+    <z-rate v-model="rate" @onclick="fn" :readonly="true"></z-rate>
   </div>
 </template>
 
 <script>
-import zProgress from '@/components/zProgress';
+import zProgress from "@/components/zProgress";
+import zRate from "@/components/zRate";
 export default {
-  components:{zProgress}
-}
+  components: { zProgress, zRate },
+  data() {
+    return {
+      rate: 3
+    };
+  },
+  methods: {
+    fn(p) {
+      console.log(p);
+    }
+  }
+};
 </script>
 
 <style>
-*{
-    padding:0;
-    margin:0;
+* {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  padding: 50px;
 }
 </style>
---
+++ src/components/zRate.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="rate-component">
+    <div
+      class="item"
+      v-for="(v,k) in list"
+      :key="k"
+      :class="{'choose':v.id <= rate}"
+      @click="fn(v.id)"
+    ></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        },
+        {
+          id: 3
+        },
+        {
+          id: 4
+        },
+        {
+          id: 5
+        }
+      ]
+    };
+  },
+  props: ["value", "readonly"],
+  computed: {
+    rate: {
+      get: function() {
+        return this.value;
+      },
+      set: function(value) {
+        this.$emit("input", value);
+      }
+    }
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    fn(id) {
+      if (!this.readonly) {
+        this.rate = id;
+        this.$emit("onclick", id);
+      }
+    }
+  }
+};
+</script>
+<style>
+.rate-component {
+  display: flex;
+}
+.item {
+  margin-right: 10px;
+  width: 30px;
+  height: 30px;
+  background: gray;
+}
+.choose {
+  background: red;
+}
+</style>
